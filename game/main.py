ae5f1c452dc7e3ac8db88231ec275d7bec8dfac8
Happyigr <78278275+Happyigr@users.noreply.github.com>
2021-03-14 15:18:52 +0100

    Gui update
    В настройках можно выйти в меню,
    в настройках работает кнопка выход,
    в улучшениях появилась кнопка

--- game/main.py
+++ game/main.py
@@ -24,14 +24,18 @@ def show_setting_screen():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for button in setting_buttons:
                     if button.isOver(mouse):
-                        if button == back_button:
+                        if button == back_button_setting:
                             setting = False
-                        if button == exit_button:
+                        if button == in_menu_button_settings:
+                            setting = False
+                            return True  # game over == True
+                        if button == exit_button_all:
                             quit()
+
         pygame.display.flip()
 
 
-def show_upgrade_screen():
+def show_shop_screen():
     upgrade = True
     while upgrade:
         clock.tick(FPS)
@@ -39,10 +43,10 @@ def show_upgrade_screen():
         screen.fill(BLACK)
         draw_text(screen, (str(all_money) + ' $'), 30, WIDTH / 2, 30)
         draw_text(screen, 'Магазин', 56, WIDTH / 2, 100)
-        for button in upgrade_buttons:
+        for button in shop_buttons:
             button.draw(screen)
         for event in pygame.event.get():
-            for button in upgrade_buttons:
+            for button in shop_buttons:
                 if button.isOver(mouse):
                     button.color = (DARK_BUTTON)
                 else:
@@ -50,11 +54,11 @@ def show_upgrade_screen():
             if event.type == pygame.QUIT:
                 quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                for button in info_buttons:
+                for button in shop_buttons:
                     if button.isOver(mouse):
-                        if button == back_button:
+                        if button == back_button_shop:
                             upgrade = False
-                        if button == buy_button:
+                        if button == buy_button_shop:
                             pass
         pygame.display.flip()
 
@@ -81,13 +85,13 @@ def show_menu_screen():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for button in menu_buttons:
                     if button.isOver(mouse):
-                        if button == exit_button:
+                        if button == exit_button_all:
                             quit()
-                        if button == info_button:
+                        if button == info_button_menu:
                             show_info_screen()
-                        if button == upgrade_button:
-                            show_upgrade_screen()
-                        if button == play_button:
+                        if button == upgrade_button_menu:
+                            show_shop_screen()
+                        if button == play_button_menu:
                             waiting = False
         pygame.display.flip()
 
@@ -117,7 +121,7 @@ def show_info_screen():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for button in info_buttons:
                     if button.isOver(mouse):
-                        if button == back_button:
+                        if button == back_button_info:
                             info = False
         pygame.display.flip()
 
@@ -516,24 +520,38 @@ all_sprites.add(speedboost)
 
 # Кнопки
 setting_buttons = []
-exit_button = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 100, 200, 50, 'Выход', 30)
-back_button = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 175, 200, 50, 'Назад', 30)
-setting_buttons.append(exit_button)
-setting_buttons.append(back_button)
 menu_buttons = []
 info_buttons = []
-upgrade_buttons = []
-info_button = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 175, 200, 50, 'Инфо', 30)
-upgrade_button = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 250, 200, 50, 'Улучшение', 30)
-play_button = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 325, 200, 50, 'Играть', 30)
-buy_button = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 250, 200, 50, 'Купить', 30)
-menu_buttons.append(info_button)
-menu_buttons.append(upgrade_button)
-menu_buttons.append(play_button)
-menu_buttons.append(exit_button)
-info_buttons.append(back_button)
-upgrade_buttons.append(back_button)
-upgrade_buttons.append(buy_button)
+shop_buttons = []
+exit_button_all = Button(WHITE, 100, HEIGHT - 100, 200, 50, 'Выход', 30)
+# Кнопки Настроек
+back_button_setting = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 250, 200, 50, 'Продолжить', 30)
+in_menu_button_settings = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 175, 200, 50, 'В меню', 30)
+# Кнопки меню
+play_button_menu = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 325, 200, 50, 'Играть', 30)
+upgrade_button_menu = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 250, 200, 50, 'Улучшение', 30)
+info_button_menu = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 175, 200, 50, 'Инфо', 30)
+# Кнопки для магазина
+back_button_shop = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 100, 200, 50, 'Назад', 30)
+buy_button_shop = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 175, 200, 50, 'Купить', 30)
+# Кнопки для инфо вкладки
+back_button_info = Button(WHITE, ((WIDTH / 2) - 100), HEIGHT - 100, 200, 50, 'Назад', 30)
+
+# Товары
+armor_1_button_shop = Button_img(armor_1_img, 300, 300)
+
+# Добавление кнопок в списки
+setting_buttons.append(exit_button_all)
+setting_buttons.append(back_button_setting)
+setting_buttons.append(in_menu_button_settings)
+menu_buttons.append(exit_button_all)
+menu_buttons.append(info_button_menu)
+menu_buttons.append(play_button_menu)
+menu_buttons.append(upgrade_button_menu)
+shop_buttons.append(back_button_shop)
+shop_buttons.append(buy_button_shop)
+shop_buttons.append(armor_1_button_shop)
+info_buttons.append(back_button_info)
 
 # Цикл игры
 new_lvl_time = 0
@@ -565,7 +583,7 @@ while running:
         # Настройки
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                show_setting_screen()
+                game_over = show_setting_screen()
             if event.key == pygame.K_e:
                 lvl_num += 1
                 new_lvl_time = time
