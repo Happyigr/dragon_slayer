96901b711a41bce05adfc00e7e497253f63b0b4a
Happyigr <78278275+Happyigr@users.noreply.github.com>
2021-03-21 23:24:22 +0100

    Wall added добавленны стены и их размер можно изменять

---
+++ game/stuff/Map.py
@@ -0,0 +1,26 @@
+import pygame
+from game.stuff.Settings import *
+
+# W = стена
+text_map = [
+    'WWWWWWWWWWWWWWW',
+    'W.............W',
+    'W.............W',
+    'W.............W',
+    'W.............W',
+    'W.............W',
+    'W.............W',
+    'W.............W',
+    'W.............W',
+    'WWWWWWWWWWWWWWW',
+]
+
+wall_colision = []
+world_map = set()
+for j, row in enumerate(text_map):
+    print(j, '    ', row)
+    for i, char in enumerate(row):
+        print(i, '    ', char)
+        if char == 'W':
+            world_map.add((i * WALL_SIZE, j * WALL_SIZE))
+            wall_colision.append(pygame.Rect(i * WALL_SIZE, j * WALL_SIZE, WALL_SIZE, WALL_SIZE))
